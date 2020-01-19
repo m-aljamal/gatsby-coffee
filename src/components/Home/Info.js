a943c3f7725a283328ab5b955d2500899a67b2b7
m-aljamal <m-aljamal@github.com>
2020-01-19 11:24:15 +0300

    editing info

--- src/components/Home/Info.js
+++ src/components/Home/Info.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import Title from "../../Globals/Title"
+import Title from "../Globals/Title"
 function Info(props) {
   return (
     <section className="py-5">
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
-import Info from "../components/Home/Home/info"
+import Info from "../components/Home/Info"
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
@@ -12,7 +12,7 @@ const IndexPage = ({ data }) => (
       title="reguler joe's "
       styleClass="default-background"
     />
-    <Info/>
+    <Info />
   </Layout>
 )
 
